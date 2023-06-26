4d72e67dba726dd1246e417f9b0fe76c4982ffb2
Dirk Väth <dirk.vaeth@ims.uni-stuttgart.de>
2023-06-26 15:52:41 +0200

    reading course name now from mbz archive

--- moodle-docker/moodle-php-apache/root/tmp/setup/plugins/get_course_id.php
+++ moodle-docker/moodle-php-apache/root/tmp/setup/plugins/get_course_id.php
@@ -1,6 +1,17 @@
 <?php
 define('CLI_SCRIPT', true);
 
+echo '=== Course ID Finder ===';
+
+// Extract backup main xml file from backup archive
+$backup_path = '/tmp/setup/data/' . getenv('COURSE_BACKUP_FILE');
+$contents = file_get_contents('phar://' . $backup_path . '/moodle_backup.xml');
+
+// Read course name from backup main xml file
+$xml=simplexml_load_string($contents);
+$course_full_name = $xml->information->original_course_fullname;
+echo "Found course '" . $course_full_name . "' in backup";
+
 $configfile = '/var/www/html/config.php';
 require($configfile);
 require_once($CFG->libdir.'/dmllib.php');
@@ -8,7 +19,7 @@ setup_DB();
 $courses = $DB->get_records('course');
 $courseid = "NULL";
 foreach($courses as $course) {  
-    if($course->fullname == "Zusatzqualifikation KI und Maschinelles Lernen") {
+    if($course->fullname == $course_full_name) {
         $courseid = $course->id;
     }
 }

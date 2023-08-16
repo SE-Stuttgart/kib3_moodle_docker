090fefb34bf26b93ca0a4a2f9458d3fc9caaa43c
Dirk Väth <dirk.vaeth@ims.uni-stuttgart.de>
2023-08-16 17:27:06 +0200

    fixed access rights & wwwroot

--- moodle-docker/moodle-php-apache/root/tmp/setup/install_moodle.sh
+++ moodle-docker/moodle-php-apache/root/tmp/setup/install_moodle.sh
@@ -2,6 +2,12 @@
 # Load environment variables that are otherwise not visible inside this script
 source /tmp/setup/config.env 
 
+# Change directory permissions
+echo "Setting directory permissons..."
+sudo chmod -R 777 /var/www/moodledata
+sudo chmod 755 /var/www/moodledata
+echo "Done"
+
 ###
 ### Check if moodle is already installed - if not, install
 ###
@@ -11,7 +17,7 @@ then
     echo "Installing moodle..."
 
     sudo chmod +x /tmp/setup/moodle-docker-wait-for-it.sh 
-    sudo -u www-data /tmp/setup/moodle-docker-wait-for-it.sh db:$MOODLE_DOCKER_DBPORT -- php /var/www/html/admin/cli/install.php --lang=de --chmod=2775 --wwwroot="http://${MOODLE_DOCKER_WEB_PORT}" --dataroot=/var/www/moodledata --adminuser=$MOODLE_ADMIN_USER --adminpass=$MOODLE_ADMIN_PASSWORD --adminemail=$MOODLE_ADMIN_EMAIL --non-interactive --allow-unstable --agree-license --dbtype=$MOODLE_DOCKER_DBTYPE --dbhost="db" --dbport=3306  --dbname=$MOODLE_DOCKER_DBNAME --dbuser=$MOODLE_DOCKER_DBUSER --dbpass=$MOODLE_DOCKER_DBPASS --fullname=$MOODLE_FULL_NAME --shortname=$MOODLE_SHORT_NAME
+    sudo -u www-data /tmp/setup/moodle-docker-wait-for-it.sh db:$MOODLE_DOCKER_DBPORT -- php /var/www/html/admin/cli/install.php --lang=de --chmod=2775 --wwwroot="http://${MOODLE_DOCKER_URL}" --dataroot=/var/www/moodledata --adminuser=$MOODLE_ADMIN_USER --adminpass=$MOODLE_ADMIN_PASSWORD --adminemail=$MOODLE_ADMIN_EMAIL --non-interactive --allow-unstable --agree-license --dbtype=$MOODLE_DOCKER_DBTYPE --dbhost="db" --dbport=$MOODLE_DOCKER_DBPORT  --dbname=$MOODLE_DOCKER_DBNAME --dbuser=$MOODLE_DOCKER_DBUSER --dbpass=$MOODLE_DOCKER_DBPASS --fullname=$MOODLE_FULL_NAME --shortname=$MOODLE_SHORT_NAME
 
     ###
     ### Configure moodle BEFORE IMPORTING COURSE

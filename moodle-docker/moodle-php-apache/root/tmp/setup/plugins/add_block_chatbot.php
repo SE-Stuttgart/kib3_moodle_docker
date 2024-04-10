4152f9adea7a256ae4823fe717db6dbf19187d0a
Dirk Väth <dirk.vaeth@ims.uni-stuttgart.de>
2024-04-10 14:30:17 +0200

    initial chatbot config using moodle settings api now

--- moodle-docker/moodle-php-apache/root/tmp/setup/plugins/add_block_chatbot.php
+++ moodle-docker/moodle-php-apache/root/tmp/setup/plugins/add_block_chatbot.php
@@ -5,32 +5,22 @@ $configfile = '/var/www/html/moodle/config.php';
 require($configfile);
 require_once($CFG->libdir.'/dmllib.php');
 setup_DB();
+require_once($CFG->libdir.'/blocklib.php');
+require_once($CFG->dirroot . "/blocks/chatbot/lib.php");
 
 // get course
 $course_id = intval(file_get_contents("/tmp/setup/data/restored_course_info.txt"));
 $course = $DB->get_record('course', array('id'=>$course_id));
 
 // activate chatbot for restored course
-if($DB->record_exists("config_plugins", array("plugin" => "block_chatbot", "name" => "courseids"))) {
-    $chatbot_course_ids_config = $DB->get_record("config_plugins", array("plugin" => "block_chatbot", "name" => "courseids"));
-    $chatbot_course_ids_config->value = strval($course->id);
-    $DB->update_record("config_plugins", $chatbot_course_ids_config);
-    echo "\nUpdated chatbot config: Activated for course " . $course->id; 
-} else {
-    $DB->insert_record("config_plugins", array(
-        "plugin" => "block_chatbot",
-        "name" => "courseids",
-        "value" => strval($course->id)
-    ));
-    echo "\nInserted chatbot config: Activated for course " . $course->id; 
-}
+$chatbot_course_ids_config = $DB->get_record("config_plugins", array("plugin" => "block_chatbot", "name" => "courseids"));
+set_config("courseids", strval($course->id), "block_chatbot");
+echo "\nUpdated chatbot config: Activated for courses " . get_config("block_chatbot", "courseids"); 
 
 // sync moodle course module views / completions with chatbot (for restored course)
-require_once($CFG->dirroot . "/blocks/chatbot/lib.php");
 sync_course_module_history($course_id);
 
 // set course page context
-require_once($CFG->libdir.'/blocklib.php');
 $page = new moodle_page();
 $page->set_course($course);
 
